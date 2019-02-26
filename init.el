77f9e970319b045066c279259028116c48395620
yasu-Ubuntu-MacbookPro <yasu313nori@gmail.com>
2019-02-26 11:08:56 +0900

    add font setting

--- init.el
+++ init.el
@@ -62,7 +62,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
 
 ;; correctly highlight ros launch files as XML files
 ;; http://wiki.ros.org/roslaunch/Tutorials/Using%20Roslaunch%20with%20Emacs
